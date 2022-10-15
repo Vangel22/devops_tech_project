dd0900997ea27b25f3f1f21a217c9fa115ca0c4d
Vangel22 <h.vangel22@gmail.com>
2022-10-15 18:45:35 +0200

    Docker setup complete

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Vangel Hristov</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Index <b>181233</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
